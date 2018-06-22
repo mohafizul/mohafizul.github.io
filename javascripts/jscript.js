bb385f60c0241ada169fbd1f04409d42699eb36d
mohafizul <mohafizul@yahoo.com>
2018-06-22 02:05:56 +0000

    modified jscript file

--- javascripts/jscript.js
+++ javascripts/jscript.js
@@ -1,10 +1,6 @@
 /*global $*/
 
 $( document ).ready(function() {
-    $("#demo1").click(function() {
-       alert("The paragraph was clicked.");
-    });
-    
     
     var popCanvas = document.getElementById("popChart");
     var lineChart = new Chart(popCanvas, {
@@ -13,12 +9,12 @@ $( document ).ready(function() {
         labels: [1,2,4,6,8,10,20,40,60,80,100],
         datasets: [{ 
           data: [99.606,120.212,143.613,158.832,170.414,179.886,212.385,250.385,275.586,295.009,310.999],
-          label: "Env1",
+          label: "APWS-IF97",
           borderColor: "#3e95cd",
           fill: false
       }, { 
         data: [100.000,118.921,141.421,156.508,168.179,177.828,211.474,251.487,278.316,299.070,316.228],
-        label: "Double Sqroot",
+        label: "Double Square Root",
         borderColor: "#c45850",
         fill: false
       }
@@ -27,7 +23,7 @@ $( document ).ready(function() {
   options: {
     title: {
       display: true,
-      text: 'Abs Pressure (barA) vs. Env1 vs. Env2 vs. Double Sqroot Temp (degC)'
+      text: 'APWS-IF97 vs. Double Square Root Formula'
     }
   }
     });
